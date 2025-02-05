cb967fc3bfe0d2d66e76e9f26b45c012d0fbd57c
Manjulatha10 <amanjulatha.levontechno@gmail.com>
2025-02-05 18:35:35 +0530

    second commit

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -11,29 +11,29 @@ const getBooks = async (req, res) => {
         }
 
 }
-const getBook = async (res, req) => {
+const getBook = async (req, res) => {
     try {
         const {id} = req.params;
         const book = await Book.findById(id);
         res.status(200).json(book)
      }
      catch(error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
      }
 };
 
-const createBook = async (res, req) => {
+const createBook = async (req, res) => {
     try{
         const book = await Book.create(req.body)
         res.status(200).json(book)
     }
     catch (error){
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
 };
 
 
-const updateBook = async (res, req) => {
+const updateBook = async (req, res) => {
     try {
         const {id} = req.params 
         const book = await Book.findByIdAndUpdate(id, req.body)
@@ -52,7 +52,7 @@ const updateBook = async (res, req) => {
 
 
 
-const deleteBook = async (res, req) => {
+const deleteBook = async (req, res) => {
     try {
         const {id} = req.params 
         const book = await Book.findByIdAndDelete(id, req.body)
--- routes/book.route.js
+++ routes/book.route.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const {getBooks, getBook, createBook, updateBook, deleteBook} = require('../controllers/book.controller.js');
 
 
-router.get('./', getBook);
+router.get('/', getBooks);
 router.get('/:id', getBook);
-router.post('./', createBook);
-router.put('./:id', updateBook);
-router.delete('./:id', deleteBook);
+router.post('/', createBook);
+router.put('/:id', updateBook);
+router.delete('/:id', deleteBook);
 
 module.exports = router;
